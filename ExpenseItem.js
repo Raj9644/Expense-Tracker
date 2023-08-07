9e0a9ad7668050bac1ea4e5ac4f6d8817880ca0f
Raj9644 <99965980+Raj9644@users.noreply.github.com>
2023-08-07 17:25:38 +0530

    Update ExpenseItem.js

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,15 +1,24 @@
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+
+  
+const clickHandler = () => {
+  console.log('Clicked !!!!');
+  );
+  
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
-      </div>
+       </div>
+  <button onClick={clickHandler}>Delete</button>
     </Card>
   );
 }
