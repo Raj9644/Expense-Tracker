99d6354ce209c695fbcf944e7f1f56d9807eb35b
Raj9644 <99965980+Raj9644@users.noreply.github.com>
2023-08-07 17:58:56 +0530

    Update ExpenseItem.js

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,26 +1,41 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
-import './ExpenseItem.css';
+import Card from '../UI/Card';
+import './ExpenseItm.css';
 
 const ExpenseItem = (props) => {
+  // State to hold the updated title
+  const [title, setTitle] = useState(props.title);
+  // State to hold the updated expense amount
+  const [amount, setAmount] = useState(props.amount);
 
-  
-const clickHandler = () => {
-  console.log('Clicked !!!!');
-  );
-  
+  // Click handler to update title
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
+  };
+
+  // Click handler to update expense amount
+  const updateAmountHandler = () => {
+    setAmount(100);
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-       </div>
-  <button onClick={clickHandler}>Delete</button>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amount}</div>
+        <div>
+          {/* Button to update the title */}
+          <button onClick={clickHandler}>Update Title</button>
+          {/* Button to update the expense amount */}
+          <button onClick={updateAmountHandler}>Change Amount to $100</button>
+        </div>
+      </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
+
